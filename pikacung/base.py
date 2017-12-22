34b5112abdb3e7f151d00bcd369884caed4ed8f7
Muhammad Gilang Januar <mgilangjanuar@gmail.com>
2017-12-22 15:11:50 +0700

    update with vhost

--- pikacung/base.py
+++ pikacung/base.py
@@ -3,9 +3,10 @@ import pika
 
 class Base():
     
-    def __init__(self, host='localhost', port=5672, username='guest', password='guest'):
+    def __init__(self, host='localhost', port=5672, virtual_host='/', username='guest', password='guest'):
         self._host = host
         self._port = port
+        self._virtual_host = virtual_host
         self._username = username
         self._password = password
         self.create_pika()
@@ -15,6 +16,7 @@ class Base():
             pika.ConnectionParameters(
                 host=self._host,
                 port=self._port,
+                virtual_host=self._virtual_host,
                 credentials=pika.PlainCredentials(self._username, self._password)
             )
         )
